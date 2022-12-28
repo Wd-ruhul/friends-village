c0ddd3737a6e8a688574896360dbd2741e34179b
Wd-ruhul <ruhul.wd@gmail.com>
2022-12-28 16:04:54 +0600

    install firebase

---
+++ src/Firebase/Firebase.Console.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAk0-CNsfsN1sZllu_eQYSQ5OUnIhjBOW8",
+  authDomain: "friendsvillage-db21b.firebaseapp.com",
+  projectId: "friendsvillage-db21b",
+  storageBucket: "friendsvillage-db21b.appspot.com",
+  messagingSenderId: "31217400505",
+  appId: "1:31217400505:web:c5c3d70ef3b011df62b968",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
+
